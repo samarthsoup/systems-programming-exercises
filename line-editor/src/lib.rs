c146afa3ecb88398b4ef6c88178961d4b0e38401
samarth <samarthkulkarni@icloud.com>
2024-01-30 22:44:57 +0530

    line-editor: prints number of lines of file

--- line-editor/src/lib.rs
+++ line-editor/src/lib.rs
@@ -1,4 +1,36 @@
-use std::io::{self, Write};
+use std::{error::Error, io::{self, Write}};
+use std::fs;
+use std::io::{BufReader, BufRead};
+
+pub fn build(mut args: impl Iterator<Item = String>) -> Result<String, &'static str> {
+    args.next();
+
+    let file_path =  match args.next() {
+        Some(arg) => arg,
+        None => return Err("didn't get a file path"),
+    };
+
+    Ok(file_path)
+}
+
+pub fn read_into_vec(file_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let file = match fs::File::open(file_path) {
+        Ok(file) => file,
+        Err(_) => {
+            fs::File::create(file_path)?;
+            fs::File::open(file_path).unwrap()
+        }
+    };
+
+    let reader: BufReader<fs::File> = BufReader::new(file);
+    let mut lines: Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        lines.push(line?);
+    }
+
+    Ok(lines)
+}
 
 pub fn process_input() -> Result<String, io::Error> {
     print!("? ");
--- line-editor/src/main.rs
+++ line-editor/src/main.rs
@@ -1,10 +1,23 @@
 use std::process;
+use std::env;
 
 fn main() {
-    loop {
+    let file_path = line_editor::build(env::args()).unwrap_or_else(|e| {
+        eprintln!("{e}");
+        process::exit(1);
+    });
+
+    let contents = line_editor::read_into_vec(&file_path).unwrap_or_else(|e|  {
+        eprintln!("{e}");
+        process::exit(1);
+    });
+
+    println!("lines: {}", contents.len());
+
+    /*loop {
         if let Err(e) = line_editor::process_input() {
             eprintln!("{e}");
             process::exit(1);
         };
-    }
+    }*/
 }
